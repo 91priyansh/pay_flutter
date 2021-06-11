dad7cfdad4fbe73cf4e000a5b857134592b2b46f
wrteam-priyansh <wrteam.priyansh@gmail.com>
2021-06-11 12:41:51 +0530

    update payment

--- lib/screen/homeScreen.dart
+++ lib/screen/homeScreen.dart
@@ -12,58 +12,74 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<PaymentItem> _paymentItems = [];
   double amount = 0.0;
 
-  String convertGPaymentConfigToString() {
-    return jsonEncode({
-      "provider": "google_pay",
-      "data": {
-        "environment": "TEST",
-        "apiVersion": 2,
-        "apiVersionMinor": 0,
-        "allowedPaymentMethods": [
-          {
-            "type": "CARD",
-            "tokenizationSpecification": {
-              "type": "PAYMENT_GATEWAY",
-              "parameters": {
-                "gateway": "example",
-                "gatewayMerchantId": "gatewayMerchantId"
-              }
-            },
-            "parameters": {
-              "allowedCardNetworks": ["VISA", "MASTERCARD"],
-              "allowedAuthMethods": ["PAN_ONLY", "CRYPTOGRAM_3DS"],
-              "billingAddressRequired": true,
-              "billingAddressParameters": {
-                "format": "FULL",
-                "phoneNumberRequired": true
-              }
-            }
-          }
-        ],
-        "merchantInfo": {
-          "merchantId": "01234567890123456789",
-          "merchantName": "Example Merchant Name"
-        },
-        "transactionInfo": {"countryCode": "US", "currencyCode": "USD"}
-      }
-    });
+  //for converting patymentconfig into string
+  //pass config for apay and gpay here
+  String convertPaymentConfigToString(Map json) {
+    return jsonEncode(json);
   }
 
-  @override
-  void initState() {
-    _paymentItems.add(PaymentItem(
-      amount: "50.0",
-      label: "Adidas Sandal",
-      status: PaymentItemStatus.final_price,
-    ));
-    _paymentItems.forEach((element) {
-      amount = amount + double.parse(element.amount);
-    });
+  final Map gpayPaymentCofig = {
+    "provider": "google_pay",
+    "data": {
+      "environment": "TEST",
+      "apiVersion": 2,
+      "apiVersionMinor": 0,
+      "allowedPaymentMethods": [
+        {
+          "type": "CARD",
+          "tokenizationSpecification": {
+            "type": "PAYMENT_GATEWAY",
+            "parameters": {"gateway": "example", "gatewayMerchantId": "gatewayMerchantId"}
+          },
+          "parameters": {
+            "allowedCardNetworks": ["VISA", "MASTERCARD"],
+            "allowedAuthMethods": ["PAN_ONLY", "CRYPTOGRAM_3DS"],
+            "billingAddressRequired": true,
+            "billingAddressParameters": {"format": "FULL", "phoneNumberRequired": true}
+          }
+        }
+      ],
+      "merchantInfo": {"merchantId": "01234567890123456789", "merchantName": "Example Merchant Name"},
+      "transactionInfo": {"countryCode": "US", "currencyCode": "USD"}
+    }
+  };
 
-    super.initState();
+  final apayConfig = {
+    "provider": "apple_pay",
+    "data": {
+      "merchantIdentifier": "merchant.com.sams.fish",
+      "displayName": "Sam's Fish",
+      "merchantCapabilities": ["3DS", "debit", "credit"],
+      "supportedNetworks": ["amex", "visa", "discover", "masterCard"],
+      "countryCode": "US",
+      "currencyCode": "USD",
+      "requiredBillingContactFields": ["post"],
+      "requiredShippingContactFields": ["post", "phone", "email", "name"],
+      "shippingMethods": [
+        {"amount": "0.00", "detail": "Available within an hour", "identifier": "in_store_pickup", "label": "In-Store Pickup"},
+        {"amount": "4.99", "detail": "5-8 Business Days", "identifier": "flat_rate_shipping_id_2", "label": "UPS Ground"},
+        {"amount": "29.99", "detail": "1-3 Business Days", "identifier": "flat_rate_shipping_id_1", "label": "FedEx Priority Mail"}
+      ]
+    }
+  };
+
+  void makePayment(PayProvider payProvider, Map paymentConfig) async {
+    //add products here
+    List<PaymentItem> paymentItems = [PaymentItem(amount: "500", status: PaymentItemStatus.final_price, label: "Product name")];
+    Pay pay = Pay([PaymentConfiguration.fromJsonString(convertPaymentConfigToString(paymentConfig))]);
+    try {
+      final userCanPay = await pay.userCanPay(PayProvider.google_pay);
+      if (userCanPay) {
+        final paymentResult = await pay.showPaymentSelector(provider: PayProvider.google_pay, paymentItems: paymentItems);
+        print(paymentResult);
+      } else {
+        //show can not make payment message
+      }
+    } catch (e) {
+      //display error
+    }
   }
 
   @override
@@ -76,65 +92,16 @@ class _HomeScreenState extends State<HomeScreen> {
           child: MaterialButton(
             child: Text("Pay kar"),
             onPressed: () async {
-              Pay pay = Pay([
-                PaymentConfiguration.fromJsonString(jsonEncode({
-                  "provider": "google_pay",
-                  "data": {
-                    "environment": "TEST",
-                    "apiVersion": 2,
-                    "apiVersionMinor": 0,
-                    "allowedPaymentMethods": [
-                      {
-                        "type": "CARD",
-                        "tokenizationSpecification": {
-                          "type": "PAYMENT_GATEWAY",
-                          "parameters": {
-                            "gateway": "example",
-                            "gatewayMerchantId": "gatewayMerchantId"
-                          }
-                        },
-                        "parameters": {
-                          "allowedCardNetworks": ["VISA", "MASTERCARD"],
-                          "allowedAuthMethods": ["PAN_ONLY", "CRYPTOGRAM_3DS"],
-                          "billingAddressRequired": true,
-                          "billingAddressParameters": {
-                            "format": "FULL",
-                            "phoneNumberRequired": true
-                          }
-                        }
-                      }
-                    ],
-                    "merchantInfo": {
-                      "merchantId": "01234567890123456789",
-                      "merchantName": "Example Merchant Name"
-                    },
-                    "transactionInfo": {
-                      "countryCode": "US",
-                      "currencyCode": "USD"
-                    }
-                  }
-                }))
-              ]);
-              try {
-                final userCanPay = await pay.userCanPay(PayProvider.google_pay);
-                if (userCanPay) {
-                  final paymentResult = await pay.showPaymentSelector(
-                      provider: PayProvider.google_pay,
-                      paymentItems: _paymentItems);
-                  showDialog(
-                      context: context,
-                      builder: (_) => AlertDialog(
-                            content: Text(paymentResult.toString()),
-                          ));
-                }
-              } catch (e) {
-                //
-              }
+              //for gogole pay
+              makePayment(PayProvider.google_pay, gpayPaymentCofig);
             },
           ),
         ));
   }
 }
+
+/*
+ */
 
 /*
 Column(
